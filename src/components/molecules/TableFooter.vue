5e2d379c34cc372c57306051851de3903a4ed58c
Alvin Arkansas <alvinarkansas@gmail.com>
2021-02-22 17:57:27 +0700

    created table footer component

---
+++ src/components/molecules/TableFooter.vue
@@ -0,0 +1,63 @@
+<template>
+  <tfoot class="w-full px-6 py-2 flex items-center justify-between">
+    <p class="text-small">
+      Showing
+      <span class="font-medium">1</span>
+      to
+      <span class="font-medium">10</span>
+      of
+      <span class="font-medium">117.390</span>
+      results
+    </p>
+
+    <div class="flex items-center text-small">
+      <span class="mr-2">Records per page</span>
+      <div class="mr-2">
+        <help-select :options="[10, 25, 50, 100]" />
+      </div>
+      <div class="flex bg-white rounded-md divide-x-2 border border-grey-4">
+        <div class="p-2">
+          <svg
+            class="w-5 h-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div class="p-2">
+          <svg
+            class="w-5 h-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  </tfoot>
+</template>
+
+<script>
+import HelpSelect from './Select.vue';
+
+export default {
+  name: 'TableFooter',
+  components: {
+    HelpSelect,
+  },
+};
+</script>
+
+<style></style>
--- src/components/templates/Table.vue
+++ src/components/templates/Table.vue
@@ -1,49 +1,57 @@
 <template>
-  <div class="overflow-x-auto rounded-lg border border-grey-4">
-    <table class="min-w-full">
-      <thead class="border-b border-grey-4 whitespace-nowrap min-w-full">
-        <tr class="bg-white uppercase">
-          <th
-            v-for="(column, i) in columns"
-            :key="i"
-            class="py-3 px-6 text-small font-medium text-grey-2"
-          >
-            <p
-              :class="[
-                { 'text-center': column.align === 'center' },
-                { 'text-right': column.align === 'right' },
-              ]"
+  <div class="overflow-x-auto overflow-y-hidden rounded-lg border border-grey-4 divide-y-2">
+    <div class="overflow-x-auto">
+      <table class="min-w-full">
+        <thead class="border-b border-grey-4 whitespace-nowrap min-w-full">
+          <tr class="bg-white uppercase">
+            <th
+              v-for="(column, i) in columns"
+              :key="i"
+              class="py-3 px-6 text-small font-medium text-grey-2"
             >
-              {{ column.label }}
-            </p>
-          </th>
-        </tr>
-      </thead>
-      <tbody class="divide-y divide-grey-4 whitespace-nowrap">
-        <tr v-for="(row, i) in rows" :key="i" class="bg-white">
-          <td v-for="(data, i) in row" :key="i" class="py-3 px-6 text-small">
-            <div
-              class="w-full grid"
-              :class="[
-                { 'place-items-center': columnAlignment(i) === 'center' },
-                { 'place-items-end': columnAlignment(i) === 'right' },
-              ]"
-            >
-              <slot :column="i" :row="row" :data="data">
-                <!-- default, when there is no slot -->
-                <p>{{ data }}</p>
-              </slot>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+              <p
+                :class="[
+                  { 'text-center': column.align === 'center' },
+                  { 'text-right': column.align === 'right' },
+                ]"
+              >
+                {{ column.label }}
+              </p>
+            </th>
+          </tr>
+        </thead>
+        <tbody class="divide-y divide-grey-4 whitespace-nowrap">
+          <tr v-for="(row, i) in rows" :key="i" class="bg-white">
+            <td v-for="(data, i) in row" :key="i" class="py-3 px-6 text-small">
+              <div
+                class="w-full grid"
+                :class="[
+                  { 'place-items-center': columnAlignment(i) === 'center' },
+                  { 'place-items-end': columnAlignment(i) === 'right' },
+                ]"
+              >
+                <slot :column="i" :row="row" :data="data">
+                  <!-- default, when there is no slot -->
+                  <p>{{ data }}</p>
+                </slot>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <table-footer />
   </div>
 </template>
 
 <script>
+import TableFooter from '../molecules/TableFooter.vue';
+
 export default {
   name: 'HelpTable',
+  components: {
+    TableFooter,
+  },
   props: {
     columns: {
       type: Array,
