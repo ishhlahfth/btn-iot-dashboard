9018817ce553767c151ddfad82d6279dcf57cfd2
Alvin Arkansas <alvinarkansas@gmail.com>
2021-03-01 16:20:31 +0700

    removed vuelidate call on main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue';
-import { VuelidatePlugin } from '@vuelidate/core';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
@@ -9,5 +8,4 @@ import './assets/tailwind.css';
 // createApp(App).use(store).use(router).mount('#app');
 createApp(App)
   .use(router)
-  .use(VuelidatePlugin)
   .mount('#app');
