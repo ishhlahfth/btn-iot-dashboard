55974dfc90c23d5db954b289e1cadff298596f0b
Even Rama <evenramac@gmail.com>
2021-03-03 13:35:24 +0700

    CI-CD fix non-existing remote dir

--- scripts/deploy-test.sh
+++ scripts/deploy-test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-scp -pqr "/tmp/${BITBUCKET_REPO_SLUG}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}/dist/*" ubuntu@staging:/home/ubuntu/bns-dashboard
+ssh ubuntu@staging "mkdir -p /home/ubuntu/bns-dashboard"
+scp -r "/tmp/${BITBUCKET_REPO_SLUG}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}/dist/." ubuntu@staging:/home/ubuntu/bns-dashboard
 rm -rf "/tmp/${BITBUCKET_REPO_SLUG}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}"
