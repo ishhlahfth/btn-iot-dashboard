daf75a7902a00f0ffb072a78d3c0a5a208b52e94
Even Rama <evenramac@gmail.com>
2021-03-03 14:03:47 +0700

    CI-CD fix deployment script

--- scripts/deploy-test.sh
+++ scripts/deploy-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ssh ubuntu@staging "mkdir -p /home/ubuntu/bns-dashboard"
-scp -r /tmp/${BITBUCKET_REPO_SLUG}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}/dist/* ubuntu@staging:/home/ubuntu/bns-dashboard
-rm -rf /tmp/${BITBUCKET_REPO_SLUG}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}
+scp -r /tmp/$BITBUCKET_REPO_SLUG/$BITBUCKET_DEPLOYMENT_ENVIRONMENT/dist/* ubuntu@staging:/home/ubuntu/bns-dashboard
+rm -rf /tmp/$BITBUCKET_REPO_SLUG/$BITBUCKET_DEPLOYMENT_ENVIRONMENT
