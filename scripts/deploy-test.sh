7581abfc58d4c311d9ec3b595e93e02fbcdde2e8
Even Rama <evenramac@gmail.com>
2021-03-03 12:47:10 +0700

    CI-CD add EXTRA_ARGS on SCP

--- scripts/deploy-test.sh
+++ scripts/deploy-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scp -r "/tmp/${BITBUCKET_REPO_SLUG}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}/dist" ubuntu@staging:/home/ubuntu/bns-dashboard
+scp -pqr "/tmp/${BITBUCKET_REPO_SLUG}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}/dist/*" ubuntu@staging:/home/ubuntu/bns-dashboard
 rm -rf "/tmp/${BITBUCKET_REPO_SLUG}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}"
