275e4e644659dce383b381c08f3694c1890f1a6d
Even Rama <evenramac@gmail.com>
2021-03-03 12:27:47 +0700

    CI-CD fix env test deployment script

--- scripts/deploy-test.sh
+++ scripts/deploy-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scp -r /tmp/${BITBUCKET_REPO_UUID}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}/dist ubuntu@staging:/home/ubuntu/bns-dashboard
-rm -rf /tmp/${BITBUCKET_REPO_UUID}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}
+scp -r "/tmp/${BITBUCKET_REPO_UUID}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}/dist" ubuntu@staging:/home/ubuntu/bns-dashboard
+rm -rf "/tmp/${BITBUCKET_REPO_UUID}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}"
