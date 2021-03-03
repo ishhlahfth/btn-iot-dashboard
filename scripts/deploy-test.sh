880ef7334b3ff10638c28392ff778a2b17da40c4
Even Rama <evenramac@gmail.com>
2021-03-03 14:27:31 +0700

    CI-CD use static path

--- scripts/deploy-test.sh
+++ scripts/deploy-test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "BITBUCKET_REPO_SLUG: ${BITBUCKET_REPO_SLUG}"
-echo "BITBUCKET_DEPLOYMENT_ENVIRONMENT: ${BITBUCKET_DEPLOYMENT_ENVIRONMENT}"
-
 ssh ubuntu@staging "mkdir -p /home/ubuntu/bns-dashboard"
-scp -r /tmp/${BITBUCKET_REPO_SLUG}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}/dist/* ubuntu@staging:/home/ubuntu/bns-dashboard
-rm -rf /tmp/${BITBUCKET_REPO_SLUG}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}
+scp -r /tmp/bns-dashboard/test/dist/* ubuntu@staging:/home/ubuntu/bns-dashboard
+rm -rf /tmp/bns-dashboard/test
