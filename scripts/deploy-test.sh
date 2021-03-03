39d6b52b20a178ecddbeb5df670d4dcbec2a6f50
Even Rama <evenramac@gmail.com>
2021-03-03 14:18:15 +0700

    CI-CD use envsubst

--- scripts/deploy-test.sh
+++ scripts/deploy-test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "BITBUCKET_REPO_SLUG: ${BITBUCKET_REPO_SLUG}"
+echo "BITBUCKET_DEPLOYMENT_ENVIRONMENT: ${BITBUCKET_DEPLOYMENT_ENVIRONMENT}"
+
 ssh ubuntu@staging "mkdir -p /home/ubuntu/bns-dashboard"
-scp -r /tmp/$BITBUCKET_REPO_SLUG/$BITBUCKET_DEPLOYMENT_ENVIRONMENT/dist/* ubuntu@staging:/home/ubuntu/bns-dashboard
-rm -rf /tmp/$BITBUCKET_REPO_SLUG/$BITBUCKET_DEPLOYMENT_ENVIRONMENT
+scp -r /tmp/${BITBUCKET_REPO_SLUG}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}/dist/* ubuntu@staging:/home/ubuntu/bns-dashboard
+rm -rf /tmp/${BITBUCKET_REPO_SLUG}/${BITBUCKET_DEPLOYMENT_ENVIRONMENT}
