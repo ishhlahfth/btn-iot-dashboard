44493ec726affdc3e4d90ca3d5eed68bf679c7c9
Alvin Arkansas <alvinarkansas@gmail.com>
2021-03-18 17:40:45 +0700

    added loading state

--- src/views/Invoice.vue
+++ src/views/Invoice.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-4 grid gap-4 relative" id="invoice">
+  <div class="p-4 grid gap-4 relative" :class="{ 'animate-pulse': loading }" id="invoice">
     <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2">
       <img src="../assets/sudah-bayar.png" alt="sudah-lunas" style="width: 75vw; max-width: none !important;">
     </div>
@@ -10,16 +10,16 @@
 
     <div class="w-full grid grid-cols-2 gap-y-4 gap-x-6 sm:gap-x-14 text-small">
       <p class="font-bold">Nomor PO</p>
-      <p class="text-royal text-right">{{ poNumber }}</p>
+      <p class="text-royal text-right" :class="{ 'bg-grey-4 rounded': loading }">{{ poNumber }}</p>
       <p class="font-bold">Nama Penjual</p>
-      <p class="text-right">{{ merchantName }}</p>
+      <p class="text-right" :class="{ 'bg-grey-4 rounded': loading }">{{ merchantName }}</p>
       <p class="font-bold">Tanggal Pembelian</p>
-      <p class="text-right">{{ orderDate }}</p>
+      <p class="text-right" :class="{ 'bg-grey-4 rounded': loading }">{{ orderDate }}</p>
       <p class="font-bold">Nama Pembeli</p>
-      <p class="text-right">{{ buyerName }}</p>
+      <p class="text-right" :class="{ 'bg-grey-4 rounded': loading }">{{ buyerName }}</p>
       <div class="col-span-2 grid gap-y-2">
         <p class="font-bold">Tujuan Pengiriman</p>
-        <div class="grid gap-y-1">
+        <div class="grid gap-y-1" :class="{ 'bg-grey-4 rounded h-4': loading }">
           <p>{{ addressShort }}</p>
           <p>{{ addressFull }}</p>
           <p>{{ phoneNumber }}</p>
@@ -43,16 +43,16 @@
         </div>
         <div v-for="item in items" :key="item.id" class="p-3 grid grid-cols-8 gap-1">
           <div class="col-span-3 grid gap-1">
-            <p class="font-bold">{{ item.name }}</p>
-            <p>{{ item.variations.map((el) => el.options[0].name).join(', ') }}</p>
+            <p class="font-bold" :class="{ 'bg-grey-4 rounded': loading }">{{ item.name }}</p>
+            <p :class="{ 'bg-grey-4 rounded': loading }">{{ item.variations.map((el) => el.options[0].name).join(', ') }}</p>
           </div>
-          <p class="col-span-1 text-right">{{ item.qty }}</p>
-          <p class="col-span-2 text-right">{{ store.methods.convertToRp(item.price) }}</p>
-          <p class="col-span-2 text-right">{{ store.methods.convertToRp(item.subtotal_price) }}</p>
+          <p class="col-span-1 text-right" :class="{ 'bg-grey-4 rounded': loading }">{{ store.methods.groupDigit(item.qty) }}</p>
+          <p class="col-span-2 text-right" :class="{ 'bg-grey-4 rounded': loading }">{{ store.methods.convertToRp(item.price) }}</p>
+          <p class="col-span-2 text-right" :class="{ 'bg-grey-4 rounded': loading }">{{ store.methods.convertToRp(item.subtotal_price) }}</p>
         </div>
         <div class="p-3 grid grid-cols-4 gap-1 font-bold">
           <p class="col-span-3 text-right">Subtotal Harga Barang</p>
-          <p class="text-right">{{ store.methods.convertToRp(subtotalItem) }}</p>
+          <p class="text-right" :class="{ 'bg-grey-4 rounded': loading }">{{ subtotalItem }}</p>
         </div>
       </div>
     </div>
@@ -61,11 +61,11 @@
       <div class="rounded-lg border border-grey-1 text-xsmall divide-y divide-grey-1">
         <div class="p-3 grid grid-cols-2 gap-1">
           <p>{{ deliveryName }}</p>
-          <p class="text-right">{{ store.methods.convertToRp(deliveryPrice) }}</p>
+          <p class="text-right" :class="{ 'bg-grey-4 rounded': loading }">{{ deliveryPrice }}</p>
         </div>
         <div class="p-3 grid grid-cols-2 gap-1 font-bold">
           <p>Subtotal Pengiriman</p>
-          <p class="text-right">{{ store.methods.convertToRp(subtotalDelivery) }}</p>
+          <p class="text-right" :class="{ 'bg-grey-4 rounded': loading }">{{ subtotalDelivery }}</p>
         </div>
       </div>
     </div>
@@ -74,7 +74,7 @@
       <div class="rounded-lg border border-grey-1 text-xsmall divide-y divide-grey-1">
         <div class="p-3 grid grid-cols-2 gap-1 font-bold">
           <p>Total Pembayaran</p>
-          <p class="text-right">{{ store.methods.convertToRp(totalPrice) }}</p>
+          <p class="text-right" :class="{ 'bg-grey-4 rounded': loading }">{{ totalPrice }}</p>
         </div>
       </div>
     </div>
@@ -100,14 +100,17 @@ export default {
     const addressFull = ref('');
     const phoneNumber = ref('');
     const items = ref([]);
-    const subtotalItem = ref(0);
+    const subtotalItem = ref('');
     const deliveryName = ref('');
-    const deliveryPrice = ref(0);
-    const subtotalDelivery = ref(0);
-    const totalPrice = ref(0);
+    const deliveryPrice = ref('');
+    const subtotalDelivery = ref('');
+    const totalPrice = ref('');
+
+    const loading = ref(false);
 
     const getInvoice = async () => {
       try {
+        loading.value = true;
         const {
           data: { data },
         } = await API.get(`orders/${route.params.id}`);
@@ -120,16 +123,15 @@ export default {
         addressFull.value = data.order_type_details.shipping_address.line_address || '-';
         phoneNumber.value = data.order_type_details.shipping_address.contact_person_hp || '-';
         items.value = data.items || [];
-        subtotalItem.value = data.subtotal_price || 0;
+        subtotalItem.value = store.methods.convertToRp(data.subtotal_price) || 'Rp 0';
         deliveryName.value = data.order_type_details.delivery_method.name || 'Unknown Delivery Method';
-        deliveryPrice.value = data.order_type_details.delivery_method.price || 0;
+        deliveryPrice.value = store.methods.convertToRp(data.order_type_details.delivery_method.price) || 'Rp 0';
         subtotalDelivery.value = deliveryPrice.value;
-        totalPrice.value = data.total_price || 0;
-
-        console.log('INVOICE', data);
+        totalPrice.value = store.methods.convertToRp(data.total_price) || 'Rp 0';
       } catch (error) {
         console.log(error);
       }
+      loading.value = false;
     };
 
     onMounted(() => {
@@ -151,6 +153,7 @@ export default {
       deliveryPrice,
       subtotalDelivery,
       totalPrice,
+      loading,
     };
   },
 };
