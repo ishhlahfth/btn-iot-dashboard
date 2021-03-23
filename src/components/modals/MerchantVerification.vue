0e7bfc9442a35c977ef7a44131380a1186538d73
Alvin Arkansas <alvinarkansas@gmail.com>
2021-03-23 09:44:26 +0700

    populated merchant verivication detail

--- src/components/modals/MerchantVerification.vue
+++ src/components/modals/MerchantVerification.vue
@@ -2,28 +2,34 @@
   <div class="grid gap-6 inner-modal-auto modal-md overflow-auto">
     <div class="flex justify-between items-center">
       <p class="text-heading4 font-semibold">Header</p>
-      <help-button icon-only icon="close" bg-color="transparent" color="grey-1" @click="$emit('close')" />
+      <help-button
+        icon-only
+        icon="close"
+        bg-color="transparent"
+        color="grey-1"
+        @click="$emit('close')"
+      />
     </div>
     <div class="grid grid-cols-2 auto-rows-max gap-4 sm:gap-6 font-medium">
       <div>
         <p class="text-grey-2">Seller</p>
-        <p>Ketoprak Mbak Yati Gienis Soole</p>
+        <p>{{ verificationDetail.name }}</p>
       </div>
       <div>
         <p class="text-grey-2">Verification Status</p>
-        <p>Pending</p>
+        <p>{{ verificationDetail.verify_status }}</p>
       </div>
       <div>
         <p class="text-grey-2">ID No. (KTP)</p>
-        <p>3510242312960004</p>
+        <p>-</p>
       </div>
       <div>
         <p class="text-grey-2">Cause of Failure</p>
-        <p>Gambar KTP buram</p>
+        <p>{{ verificationDetail.verify_reason || '-' }}</p>
       </div>
       <div class="col-span-2">
         <p class="text-grey-2">ID Card</p>
-        <help-thumbnail width="100%" :height="store.state.screenWidth < 640 ? 196 : 248" />
+        <help-thumbnail width="100%" :height="screenWidth < 640 ? 196 : 248" />
       </div>
     </div>
     <div class="flex flex-col sm:flex-row-reverse">
@@ -38,14 +44,20 @@ import HelpButton from '@/components/atoms/Button.vue';
 import HelpThumbnail from '@/components/atoms/Thumbnail.vue';
 
 export default {
-  name: 'SellerVerification',
+  name: 'MerchantVerification',
   components: {
     HelpButton,
     HelpThumbnail,
   },
   setup() {
-    const store = inject('store');
-    return { store };
+    const {
+      state: {
+        screenWidth,
+        modalState: { verificationDetail },
+      },
+    } = inject('store');
+
+    return { screenWidth, verificationDetail };
   },
 };
 </script>
