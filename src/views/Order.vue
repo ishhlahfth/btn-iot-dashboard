203a0e0fe4df054681596abc51a6a6641be2f90c
Alvin Arkansas <alvinarkansas@gmail.com>
2021-03-30 16:36:48 +0700

    removed transfers menu's columns, added order status column

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -2,7 +2,7 @@
   <div class="p-4 sm:p-6 grid gap-4 sm:gap-6">
     <div class="w-full flex justify-between">
       <p class="text-heading2 font-semibold">Order</p>
-      <div class="grid grid-flow-col gap-4">
+      <!-- <div class="grid grid-flow-col gap-4">
         <help-button
           v-if="!transferMode"
           label="transfer"
@@ -13,7 +13,7 @@
           <help-button label="cancel" bg-color="flame" @click="transferMode = false" />
           <help-button label="transfer" bg-color="mint" @click="transferMode = !transferMode" />
         </template>
-      </div>
+      </div> -->
     </div>
     <!-- <div>
       <form @submit.prevent="getOrders">
@@ -34,11 +34,25 @@
         @onChangePagination="getOrders($event)"
         @sort="getOrders($event)"
       >
-        <template v-slot:header="{ column: { field } }">
+        <!-- <template v-slot:header="{ column: { field } }">
           <help-checkbox v-if="field === 'checkbox'" />
-        </template>
+        </template> -->
         <template v-slot:body="{ column, row }">
-          <help-checkbox v-if="column === 'checkbox'" />
+          <!-- <help-checkbox v-if="column === 'checkbox'" /> -->
+          <div class="grid grid-flow-col auto-cols-max gap-2" v-if="column === 'current_step'">
+            <help-badge
+              :label="row.current_step"
+              :color="
+                row.current_step === 'Pesanan Selesai'
+                  ? 'positive'
+                  : row.current_step === 'Sedang Dikirim' ||
+                    row.current_step === 'Menunggu Konfirmasi'
+                  ? 'warning'
+                  : 'negative'
+              "
+              @click="openMerchantVerivication(row)"
+            />
+          </div>
           <p
             v-if="column === 'detail'"
             class="text-royal font-medium cursor-pointer"
@@ -53,8 +67,9 @@
 </template>
 
 <script>
-import HelpButton from '@/components/atoms/Button.vue';
-import HelpCheckbox from '@/components/atoms/Checkbox.vue';
+import HelpBadge from '@/components/atoms/Badge.vue';
+// import HelpButton from '@/components/atoms/Button.vue';
+// import HelpCheckbox from '@/components/atoms/Checkbox.vue';
 // import HelpInput from '@/components/atoms/Input.vue';
 import HelpTable from '@/components/templates/Table.vue';
 import mixin from '@/mixin';
@@ -65,14 +80,26 @@ export default {
   name: 'Order',
   mixins: [mixin],
   components: {
-    HelpButton,
-    HelpCheckbox,
+    HelpBadge,
+    // HelpButton,
+    // HelpCheckbox,
     // HelpInput,
     HelpTable,
   },
   data() {
     return {
       searchValue: '',
+      columns: [
+        { field: 'date', label: 'order date' },
+        { field: 'code', label: 'PO Number' },
+        { field: 'current_step', label: 'status', align: 'center' },
+        { field: 'merchant_name', label: 'merchant name' },
+        { field: 'customer_name', label: 'buyer name' },
+        { field: 'commission_fee', label: 'commission' },
+        { field: 'delivery_price', label: 'delivery price' },
+        { field: 'payment_method', label: 'payment method' },
+        { field: 'detail', label: 'detail', align: 'center' },
+      ],
       transferMode: false,
       date: '',
       orders: [],
@@ -85,35 +112,28 @@ export default {
       loading: false,
     };
   },
-  computed: {
-    columns() {
-      const columns = [
-        { field: 'date', label: 'order date', sortable: true },
-        { field: 'code', label: 'PO Number', sortable: true },
-        { field: 'merchant_name', label: 'merchant name', sortable: true },
-        { field: 'customer_name', label: 'buyer name', sortable: true },
-        {
-          field: 'transfer_status',
-          label: 'transfer status',
-          align: 'center',
-          sortable: true,
-        },
-        { field: 'failure_reason', label: 'failure reason' },
-        { field: 'subtotal_price', label: 'item price', sortable: true },
-        { field: 'commission_fee', label: 'commission', sortable: true },
-        { field: 'delivery_price', label: 'delivery price', sortable: true },
-        { field: 'payment_method', label: 'payment method', sortable: true },
-        { field: 'updated_at', label: 'last updated', sortable: true },
-        { field: 'updated_by', label: 'updated by', sortable: true },
-        { field: 'detail', label: 'detail', align: 'center' },
-        // { field: 'actions', label: ' ' },
-      ];
-      if (this.transferMode) {
-        columns.unshift({ field: 'checkbox', label: 'checkbox', align: 'center' });
-      }
-      return columns;
-    },
-  },
+  // computed: {
+  //   columns() {
+  //     const columns = [
+  //       { field: 'date', label: 'order date' },
+  //       { field: 'code', label: 'PO Number' },
+  //       { field: 'current_step', label: 'status', align: 'center' },
+  //       { field: 'merchant_name', label: 'merchant name' },
+  //       { field: 'customer_name', label: 'buyer name' },
+  //       // { field: 'failure_reason', label: 'failure reason' },
+  //       // { field: 'subtotal_price', label: 'item price' },
+  //       { field: 'commission_fee', label: 'commission' },
+  //       { field: 'delivery_price', label: 'delivery price' },
+  //       { field: 'payment_method', label: 'payment method' },
+  //       { field: 'detail', label: 'detail', align: 'center' },
+  //       // { field: 'actions', label: ' ' },
+  //     ];
+  //     // if (this.transferMode) {
+  //     //   columns.unshift({ field: 'checkbox', label: 'checkbox', align: 'center' });
+  //     // }
+  //     return columns;
+  //   },
+  // },
   methods: {
     async getOrders(pagination) {
       const limit = pagination.limit || 10;
@@ -132,6 +152,7 @@ export default {
           id: el.id,
           code: el.code,
           date: dayjs(el.date).format('DD-MM-YYYY HH:mm:ss') || '-',
+          current_step: el.current_step.title,
           merchant_name: el.merchant?.name,
           customer_name: el.customer?.profile?.name,
           commission_fee: this.convertToRp(el.commission_fee),
@@ -139,9 +160,6 @@ export default {
           delivery_price: this.convertToRp(el.order_type_details?.delivery_method?.price),
           payment_method: el.payment.name,
         }));
-
-        const whwhwhw = await API.get(`transfer-queues?offset=${offset}&limit=${limit}&sort=${sort}&order=${order}`);
-        console.log('🍌', whwhwhw);
 
         this.orderPagination = {
           limit,
