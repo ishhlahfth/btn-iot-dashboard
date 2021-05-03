0b29297c19a53a81ded22bd8551b311794ef7456
Alvin Arkansas <alvinarkansas@gmail.com>
2021-05-03 17:18:22 +0700

    created new component card

---
+++ src/components/templates/_Component.vue
@@ -0,0 +1,62 @@
+<template>
+  <section :id="id" class="grid gap-8 auto-rows-max">
+    <p class="text-heading2 font-semibold text-midnight">{{ title }}</p>
+
+    <slot name="description">-</slot>
+
+    <div class="divide-y divide-grey-4 grid gap-2">
+      <p class="font-semibold">Design</p>
+      <!-- <div class="flex justify-between">
+        <p class="font-semibold">Design</p>
+        <help-button icon-only icon="code" bg-color="transparent" color="grey-1" />
+      </div> -->
+      <p></p>
+    </div>
+
+    <slot name="design">-</slot>
+
+    <div class="divide-y divide-grey-4 grid gap-2">
+      <p class="font-semibold">Props</p>
+      <p></p>
+    </div>
+    <help-table
+      :footer="false"
+      :columns="[
+        { field: 'prop', label: 'Prop' },
+        { field: 'description', label: 'Description' },
+        { field: 'type', label: 'Type' },
+        { field: 'default', label: 'Default' },
+        { field: 'examples', label: 'Examples' },
+      ]"
+      :rows="props"
+    >
+      <template v-slot:body="{ column, row }">
+        <div v-if="column === 'examples'">
+          <code
+            v-for="(example, index) in row.examples"
+            :key="index"
+            class="rounded bg-grey-4 bg-opacity-50 mr-1 px-2"
+          >
+            {{ example }}
+          </code>
+        </div>
+      </template>
+    </help-table>
+  </section>
+</template>
+
+<script>
+// import HelpButton from '@/components/atoms/Button.vue';
+import HelpTable from '@/components/templates/Table.vue';
+
+export default {
+  name: 'Component',
+  props: ['id', 'title', 'props'],
+  components: {
+    // HelpButton,
+    HelpTable,
+  },
+};
+</script>
+
+<style></style>
--- src/views/_Components.vue
+++ src/views/_Components.vue
@@ -42,7 +42,7 @@
 
     <div class="col-span-10 h-screen overflow-auto">
       <div class="p-8 grid gap-12 auto-rows-min">
-        <section id="avatar" class="grid gap-8 auto-rows-max">
+        <!-- <section id="avatar" class="grid gap-8 auto-rows-max">
           <p class="text-heading2 font-semibold text-midnight">Avatar</p>
           <p>
             Komponen buat nampilin thumbnail yg bulet. Sebenernya fungsinya kayak komponen
@@ -74,8 +74,8 @@
               :size="32"
             />
             <help-avatar src="" :size="32" placeholder="Putri" />
-          </div>
-          <!-- <pre class="text-white bg-grey-1 rounded p-4 overflow-x-auto">
+          </div> -->
+        <!-- <pre class="text-white bg-grey-1 rounded p-4 overflow-x-auto">
 <code>&lt;<span class="tag">help-avatar</span> <span class="prop">src</span>=<span class="string">""</span> :<span class="prop">size</span>="32" <span class="prop">placeholder</span>=<span class="string">"Putri"</span> /&gt;</code>
 <code>&lt;<span class="tag">help-avatar</span>
   <span class="prop">src</span>=<span class="string">"https://www.whiteboardjournal.com/wp-content/uploads/2020/07/Fathia-12.jpg"</span>
@@ -90,7 +90,7 @@
 /&gt;</code>
 </pre> -->
 
-          <div class="divide-y divide-grey-4 grid gap-2">
+        <!-- <div class="divide-y divide-grey-4 grid gap-2">
             <p class="font-semibold">Props</p>
             <p></p>
           </div>
@@ -123,97 +123,124 @@
               },
             ]"
           />
-        </section>
+        </section> -->
 
-        <section id="badge" class="grid gap-8 auto-rows-max">
-          <p class="text-heading2 font-semibold text-midnight">Badge</p>
-          <p>
-            Pas untuk display hal-hal yg tipenya boolean atau enum. Misalnya status seperti
-            <i>Terverifikasi</i>, <i>Pending</i>, <i>Verifikasi Gagal</i>, dan <i>Akun disabled</i>
-          </p>
+        <help-component id="avatar" title="Avatar" :props="avatarProps">
+          <template v-slot:description>
+            <p>
+              Komponen buat nampilin thumbnail yg bulet. Sebenernya fungsinya kayak komponen
+              <a href="#thumbnail">thumbnail</a>, cuma ini lebih
+              straightforward dan spesifik buat avatar kayak avatar admin atau logo partner aja.
+            </p>
+          </template>
 
-          <div class="divide-y divide-grey-4 grid gap-2">
-            <div class="flex justify-between">
-              <p class="font-semibold">Design</p>
-              <help-button icon-only icon="code" bg-color="transparent" color="grey-1" />
+          <template v-slot:design>
+            <div class="grid grid-flow-col auto-cols-max gap-1">
+              <help-avatar
+                src="https://img.okezone.com/content/2017/07/11/205/1734089/rhythm-of-people-jadi-ajang-iga-massardi-kolaborasi-bareng-kikan-hingga-coki-netral-8pUCnHgLmx.jpg"
+              />
+              <help-avatar
+                src="https://www.whiteboardjournal.com/wp-content/uploads/2020/07/Fathia-12.jpg"
+              />
+              <help-avatar :src="require('@/assets/partnerlogo/OVO-logo-circle.png')" :size="40" />
+              <help-avatar
+                :src="require('@/assets/partnerlogo/lalamove-logo-circle.png')"
+                :size="40"
+              />
+              <help-avatar
+                src="https://nos.jkt-1.neo.id/mcdonalds/assets/ico/richlink.jpg"
+                :size="32"
+              />
+              <help-avatar src="" :size="32" placeholder="Admin" />
             </div>
-            <p></p>
-          </div>
+          </template>
+        </help-component>
 
-          <div class="grid grid-flow-col auto-cols-max gap-1">
-            <help-badge label="Rp 300.120" color="positive" icon="chevron-up" />
-            <help-badge label="Rp 300.120" icon="chevron-down" />
-            <help-badge label="Verivied" color="positive" />
-            <help-badge label="Not Verivied" />
-          </div>
+        <help-component id="badge" title="Badge" :props="badgeProps">
+          <template v-slot:description>
+            <p>
+              Pas untuk display hal-hal yg tipenya boolean atau enum. Misalnya status seperti
+              <i>Terverifikasi</i>, <i>Pending</i>, <i>Verifikasi Gagal</i>, dan
+              <i>Akun disabled</i>
+            </p>
+          </template>
 
-          <div class="divide-y divide-grey-4 grid gap-2">
-            <p class="font-semibold">Props</p>
-            <p></p>
-          </div>
-          <help-table
-            :footer="false"
-            :columns="[
-              { field: 'prop', label: 'Prop' },
-              { field: 'description', label: 'Description' },
-              { field: 'type', label: 'Type' },
-              { field: 'default', label: 'Default' },
-              { field: 'examples', label: 'Examples' },
-            ]"
-            :rows="[
-              {
-                prop: 'label',
-                description: 'Isinya',
-                type: 'String',
-                default: '',
-              },
-              {
-                prop: 'icon',
-                description: 'Kalo di kirinya mau ada icon. Referensinya icon name',
-                type: 'String',
-                default: '',
-                examples: ['chevron-up', 'chevron-down'],
-              },
-              {
-                prop: 'color',
-                description: 'Tema badgenya. Sementara cuma 3 presetnya',
-                type: 'String',
-                default: 'negative',
-                examples: ['positive', 'negative', 'warning'],
-              },
-            ]"
-          >
-            <template v-slot:body="{ column, row }">
-              <div v-if="column === 'examples'">
-                <code
-                  v-for="(example, index) in row.examples"
-                  :key="index"
-                  class="rounded bg-grey-4 bg-opacity-50 mr-1 px-2"
-                >
-                  {{ example }}
-                </code>
-              </div>
-            </template>
-          </help-table>
-        </section>
+          <template v-slot:design>
+            <div class="grid grid-flow-col auto-cols-max gap-1">
+              <help-badge label="Rp 300.120" color="positive" icon="chevron-up" />
+              <help-badge label="Rp 300.120" icon="chevron-down" />
+              <help-badge label="Verivied" color="positive" />
+              <help-badge label="Not Verivied" />
+            </div>
+          </template>
+        </help-component>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import HelpComponent from '@/components/templates/_Component.vue';
 import HelpAvatar from '@/components/atoms/Avatar.vue';
 import HelpBadge from '@/components/atoms/Badge.vue';
-import HelpButton from '@/components/atoms/Button.vue';
-import HelpTable from '@/components/templates/Table.vue';
+// import HelpButton from '@/components/atoms/Button.vue';
+// import HelpTable from '@/components/templates/Table.vue';
 
 export default {
   name: 'Components',
   components: {
+    HelpComponent,
     HelpAvatar,
     HelpBadge,
-    HelpButton,
-    HelpTable,
+    // HelpButton,
+    // HelpTable,
+  },
+  data() {
+    return {
+      avatarProps: [
+        {
+          prop: 'size',
+          description: 'Diameter avatar, akan dianggap px',
+          type: 'Number',
+          default: 64,
+        },
+        {
+          prop: 'src',
+          description: 'Image yang akan ditampilkan',
+          type: 'String',
+          default: '',
+          examples: ['https://...', "require('@/assets/...')"],
+        },
+        {
+          prop: 'placeholder',
+          description: 'Text untuk diambil index ke-0-nya apabila src tidak ada',
+          type: 'String',
+          default: '',
+        },
+      ],
+      badgeProps: [
+        {
+          prop: 'label',
+          description: 'Isinya',
+          type: 'String',
+          default: '',
+        },
+        {
+          prop: 'icon',
+          description: 'Kalo di kirinya mau ada icon. Referensinya icon name',
+          type: 'String',
+          default: '',
+          examples: ['chevron-up', 'chevron-down'],
+        },
+        {
+          prop: 'color',
+          description: 'Tema badgenya. Sementara cuma 3 presetnya',
+          type: 'String',
+          default: 'negative',
+          examples: ['positive', 'negative', 'warning'],
+        },
+      ],
+    };
   },
 };
 </script>
