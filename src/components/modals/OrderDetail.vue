04ce8b1750818f70c8c640005b577ea005f55e16
Alvin Arkansas <alvinarkansas@gmail.com>
2021-05-28 17:16:59 +0700

    added loading states

--- src/components/modals/OrderDetail.vue
+++ src/components/modals/OrderDetail.vue
@@ -1,91 +1,83 @@
 <template>
   <div
     class="grid grid-flow-row sm:grid-flow-col gap-6 merchant-modal-content inner-modal-fixed modal-xl overflow-auto"
-    :class="{ 'animate-pulse': loading }"
   >
     <div class="grid lg:grid-flow-col gap-8 lg:grid-cols-12 overflow-auto">
       <div class="lg:col-span-2 lg:block grid gap-4 lg:gap-8">
         <div>
-          <template v-if="!loading">
-            <div class="divide-y divide-grey-4 font-medium">
-              <p class="font-medium pb-4">Order</p>
-              <div class="pt-4 grid sm:grid-rows-2 lg:grid-rows-4 gap-4">
-                <div>
-                  <p class="text-heading1 font-semibold">{{ order.code }}</p>
-                </div>
-                <div>
-                  <p class="text-grey-2">Status</p>
-                  <p
-                    :class="
-                      order.current_step?.title === 'Pesanan Selesai'
-                        ? 'text-mint'
-                        : order.current_step?.title === 'Menunggu Konfirmasi' ||
-                          order.current_step?.title === 'Menunggu Pembayaran' ||
-                          order.current_step?.title === 'Mengajukan Komplain' ||
-                          order.current_step?.title === 'Sedang Dikirim' ||
-                          order.current_step?.title === 'Pesanan Tiba'
-                        ? 'text-gold'
-                        : 'text-flame'
-                    "
-                  >
-                    {{ order.current_step?.title }}
-                  </p>
-                </div>
-                <div>
-                  <p class="text-grey-2">Order Date</p>
-                  <p>{{ formatTime(order.date) }}</p>
-                </div>
+          <div class="divide-y divide-grey-4 font-medium">
+            <p class="font-medium pb-4">Order</p>
+            <div class="pt-4 grid sm:grid-rows-2 lg:grid-rows-4 gap-4">
+              <div>
+                <p v-if="!loading" class="text-heading1 font-semibold">{{ order.code }}</p>
+                <div v-else class="h-8 rounded bg-grey-4 animate-pulse" />
+              </div>
+              <div>
+                <p class="text-grey-2">Status</p>
+                <p
+                  v-if="!loading"
+                  :class="
+                    order.current_step?.title === 'Pesanan Selesai'
+                      ? 'text-mint'
+                      : order.current_step?.title === 'Menunggu Konfirmasi' ||
+                        order.current_step?.title === 'Menunggu Pembayaran' ||
+                        order.current_step?.title === 'Mengajukan Komplain' ||
+                        order.current_step?.title === 'Sedang Dikirim' ||
+                        order.current_step?.title === 'Pesanan Tiba'
+                      ? 'text-gold'
+                      : 'text-flame'
+                  "
+                >
+                  {{ order.current_step?.title }}
+                </p>
+                <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
+              </div>
+              <div>
+                <p class="text-grey-2">Order Date</p>
+                <p v-if="!loading">{{ formatTime(order.date) }}</p>
+                <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
               </div>
             </div>
-          </template>
-          <template v-else>
-            <div v-for="i in 6" :key="i" class="divide-y divide-grey-4">
-              <div class="rounded bg-grey-4 h-4 w-32 mb-4" />
-              <div class="pt-4 grid grid-cols-2 lg:grid-cols-4 gap-4">
-                <div class="rounded bg-grey-4 h-4" />
-                <div class="rounded bg-grey-4 h-4" />
-                <div class="rounded bg-grey-4 h-4" />
-                <div class="rounded bg-grey-4 h-4" />
-              </div>
-            </div>
-          </template>
+          </div>
         </div>
         <div class="divide-y divide-grey-4">
           <p class="font-medium pb-4">Delivery</p>
-          <div class="pt-4 grid grid-cols-2 lg:grid-cols-none gap-4 lg:gap-8">
+          <div class="pt-4 grid grid-cols-2 lg:grid-cols-none gap-4">
             <div>
               <p class="font-medium text-grey-2">Delivery PO Number</p>
-              <p class="font-medium">{{ order.delivery_code ? order.delivery_code : '-' }}</p>
+              <p v-if="!loading" class="font-medium">
+                {{ order.delivery_code ? order.delivery_code : '-' }}
+              </p>
+              <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
             </div>
             <div>
               <p class="font-medium text-grey-2">Delivery Type</p>
-              <p class="font-medium">{{ order.order_type_details?.delivery_method?.name }}</p>
+              <p v-if="!loading" class="font-medium">
+                {{ order.order_type_details?.delivery_method?.name }}
+              </p>
+              <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
             </div>
             <template v-if="order.order_type_details?.driver?.driver_name">
               <div class="col-span-2 lg:col-span-1">
-                <p class="text-grey-2">Driver</p>
-                <template v-if="screenWidth < 640">
-                  <div class="grid grid-flow-col auto-cols-max gap-2">
-                    <help-avatar
-                      :src="order.order_type_details?.driver?.driver_photo"
-                      :size="80"
-                      class="my-2"
-                    />
-                    <div>
-                      <p>{{ order.order_type_details?.driver?.driver_name }}</p>
-                      <p>{{ order.order_type_details?.driver?.vehicle_number }}</p>
-                    </div>
-                  </div>
-                </template>
-                <template v-else>
-                  <p>{{ order.order_type_details?.driver?.driver_name }}</p>
-                  <help-avatar
-                    :src="order.order_type_details?.driver?.driver_photo"
-                    :size="80"
-                    class="my-2"
-                  />
-                  <p>{{ order.order_type_details?.driver?.vehicle_number }}</p>
-                </template>
+                <p class="font-medium text-grey-2">Driver</p>
+                <p v-if="!loading" class="font-medium text-grey-2">
+                  {{ order.order_type_details?.driver?.driver_name }}
+                </p>
+                <div v-else class="h-4 mb-2 rounded bg-grey-4 animate-pulse" />
+                <help-avatar
+                  v-if="!loading"
+                  :src="order.order_type_details?.driver?.driver_photo"
+                  :size="80"
+                  class="my-2"
+                />
+                <div v-else class="h-20 w-20 rounded-full bg-grey-4 animate-pulse" />
+              </div>
+              <div>
+                <p class="font-medium text-grey-2">License number</p>
+                <p v-if="!loading" class="font-medium">
+                  {{ order.order_type_details?.driver?.vehicle_number }}
+                </p>
+                <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
               </div>
             </template>
           </div>
@@ -99,19 +91,26 @@
               <div class="grid grid-flow-row gap-4">
                 <div>
                   <p class="text-grey-2">Name</p>
-                  <p>{{ order.merchant?.name }}</p>
+                  <p v-if="!loading">{{ order.merchant?.name }}</p>
+                  <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
                 </div>
                 <div>
                   <p class="text-grey-2">Phone Number</p>
-                  <p>{{ order.merchant?.phone_number }}</p>
+                  <p v-if="!loading">{{ order.merchant?.phone_number }}</p>
+                  <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
                 </div>
                 <div class="lg:col-span-2">
                   <p class="text-grey-2">Address</p>
-                  <p>
+                  <p v-if="!loading">
                     {{
                       `${order.merchant?.address?.line_address}, ${order.merchant?.address?.district}, ${order.merchant?.address?.city}`
                     }}
                   </p>
+                  <template v-else>
+                    <div class="h-4 rounded bg-grey-4 animate-pulse mb-2" />
+                    <div class="h-4 rounded bg-grey-4 animate-pulse mb-2" />
+                    <div class="h-4 rounded w-16 bg-grey-4 animate-pulse" />
+                  </template>
                 </div>
               </div>
             </div>
@@ -122,11 +121,13 @@
             <div class="pt-4 grid gap-4">
               <div>
                 <p class="text-grey-2">Name</p>
-                <p>{{ order.customer?.profile?.name }}</p>
+                <p v-if="!loading">{{ order.customer?.profile?.name }}</p>
+                <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
               </div>
               <div>
                 <p class="text-grey-2">Phone Number</p>
-                <p>{{ order.customer?.profile?.phone_number }}</p>
+                <p v-if="!loading">{{ order.customer?.profile?.phone_number }}</p>
+                <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
               </div>
             </div>
           </div>
@@ -137,22 +138,35 @@
               <div class="grid grid-flow-row gap-4">
                 <div>
                   <p class="text-grey-2">Name</p>
-                  <p>{{ order.order_type_details?.shipping_address?.contact_person }}</p>
+                  <p v-if="!loading">
+                    {{ order.order_type_details?.shipping_address?.contact_person }}
+                  </p>
+                  <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
                 </div>
                 <div>
                   <p class="text-grey-2">Phone Number</p>
-                  <p>{{ order.order_type_details?.shipping_address?.contact_person_hp }}</p>
+                  <p v-if="!loading">
+                    {{ order.order_type_details?.shipping_address?.contact_person_hp }}
+                  </p>
+                  <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
                 </div>
                 <div class="lg:col-span-2">
                   <p class="text-grey-2">Shipping Address</p>
-                  <p>
-                    {{
-                      `${order.order_type_details?.shipping_address.line_address}, ${order.order_type_details?.shipping_address.district}, ${order.order_type_details?.shipping_address.city}`
-                    }}
-                  </p>
-                  <p class="font-light text-grey-2">
-                    {{ order.order_type_details?.shipping_address.location.coordinates }}
-                  </p>
+                  <tempalte v-if="!loading">
+                    <p>
+                      {{
+                        `${order.order_type_details?.shipping_address.line_address}, ${order.order_type_details?.shipping_address.district}, ${order.order_type_details?.shipping_address.city}`
+                      }}
+                    </p>
+                    <p class="font-light text-grey-2">
+                      {{ order.order_type_details?.shipping_address.location.coordinates }}
+                    </p>
+                  </tempalte>
+                  <template v-else>
+                    <div class="h-4 rounded bg-grey-4 animate-pulse mb-2" />
+                    <div class="h-4 rounded bg-grey-4 animate-pulse mb-2" />
+                    <div class="h-4 rounded w-16 bg-grey-4 animate-pulse" />
+                  </template>
                 </div>
               </div>
             </div>
@@ -166,7 +180,9 @@
                 <div class="flex flex-col justify-between">
                   <div class="grid">
                     <p class="font-medium text-grey-1">{{ row.name }}</p>
-                    <p class="textxsm">{{ row.variations.map((el) => el.options[0].name).join(', ') }}</p>
+                    <p class="textxsm">
+                      {{ row.variations.map((el) => el.options[0].name).join(', ') }}
+                    </p>
                   </div>
                   <div>
                     <span v-if="row.note" class="px-2 py-1 border rounded text-xsmall w-auto">
@@ -184,21 +200,31 @@
         <div class="grid md:place-content-end gap-2">
           <div class="grid gap-8 grid-cols-2">
             <span>Delivery fee</span>
-            <span class="font-medium text-right">
+            <span v-if="!loading" class="font-medium text-right">
               {{ convertToRp(order.order_type_details?.delivery_method.price) }}
             </span>
+            <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
           </div>
           <div class="grid gap-8 grid-cols-2">
             <span>Item total price</span>
-            <span class="font-medium text-right">{{ convertToRp(order?.subtotal_price) }}</span>
+            <span v-if="!loading" class="font-medium text-right">
+              {{ convertToRp(order?.subtotal_price) }}
+            </span>
+            <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
           </div>
           <div class="grid gap-8 grid-cols-2">
             <span>Commission</span>
-            <span class="font-medium text-right">{{ convertToRp(order?.commission_fee) }}</span>
+            <span v-if="!loading" class="font-medium text-right">
+              {{ convertToRp(order?.commission_fee) }}
+            </span>
+            <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
           </div>
           <div class="grid gap-8 grid-cols-2">
             <span>Grand total</span>
-            <span class="font-bold text-right">{{ convertToRp(order?.total_price) }}</span>
+            <span v-if="!loading" class="font-bold text-right">
+              {{ convertToRp(order?.total_price) }}
+            </span>
+            <div v-else class="h-4 rounded bg-grey-4 animate-pulse" />
           </div>
         </div>
       </div>
