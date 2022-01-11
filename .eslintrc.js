9ca93116c9d7bbe6aa0609f8398d2776191bf82b
ahmadwaluyo <ahmadw@wehelpyou.id>
2022-01-11 15:04:32 +0700

    configure jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: ['plugin:vue/vue3-essential', '@vue/airbnb'],
+
   parserOptions: {
     parser: 'babel-eslint',
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
@@ -22,4 +26,16 @@ module.exports = {
     'no-param-reassign': 'off',
     'import/no-cycle': 'off',
   },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
