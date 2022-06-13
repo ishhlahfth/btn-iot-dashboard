d50dd8d232ea724b352450724fc2819581cd42fe
Ishlah Alfath <muhammadishlah@wehelpyou.id>
2022-06-13 13:38:33 +0700

    fix typo

--- src/store/index.js
+++ src/store/index.js
@@ -120,7 +120,7 @@ export default createStore({
       try {
         const {
           data: { data },
-        } = await API.get(`/api/landing-page/agent-type-category`);
+        } = await API.get(`merchants/${merchantId}`);
 
         merchant = {
           merchant_id: data.id,
